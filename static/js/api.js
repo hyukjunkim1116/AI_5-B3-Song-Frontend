49f9d2c6d7ee4bc36d166daab5ec5bcaa193a2b7
hyukjunkim1116 <nmkk1234@naver.com>
2023-05-25 20:50:57 +0900

    ✨Feat: 새로운 기능 추가: article crud
    article crud 기능 추가, 사진 삭제기능 추가, 권한에 따라 사진 업로드 아이콘 안보이게 설정

--- static/js/api.js
+++ static/js/api.js
@@ -89,7 +89,6 @@ async function getUser() {
 	);
 	if (response.status == 200) {
 		response_json = await response.json();
-		console.log(response_json);
 		return response_json;
 	} else {
 		alert(response.statusText);
@@ -154,6 +153,9 @@ const handleArticlePhotoUploadBtn = () => {
 	let articleParams = getParams.split("=")[1];
 	getArticleUploadURL(articleParams);
 };
+function uploadPhoto(article_id) {
+	window.location.href = `${frontend_base_url}/upload_photo.html?article_id=${article_id}`;
+}
 
 // 모든 게시글 불러오기
 async function getArticles() {
@@ -166,9 +168,12 @@ async function getArticles() {
 
 // 특정 게시글 불러오기
 async function getArticle(article_id) {
-	const response = await fetch(`${backend_base_url}/api/articles/${article_id}/`, {
-		method: "GET"
-	});
+	const response = await fetch(
+		`${backend_base_url}/api/articles/${article_id}/`,
+		{
+			method: "GET"
+		}
+	);
 	response_json = await response.json();
 	return response_json;
 }

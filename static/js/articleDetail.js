a449436a20ab9b7d5220169667081a6095d5d544
minjeong <ehdro418@naver.com>
2023-05-26 21:41:16 +0900

    :sparkles:Feat: Add word break & Change line #4

--- static/js/articleDetail.js
+++ static/js/articleDetail.js
@@ -8,19 +8,19 @@ function articleUpdate(article_id) {
 }
 
 async function loadComments(article_id) {
-    const response = await getArticleComments(article_id);
-    const payload = JSON.parse(localStorage.getItem("payload"));
-    if (payload) {
-        currentUserId = payload.nickname;
-    } else {
-        currentUserId = null;
-    }
-    console.log(payload);
-    const commentsList = document.getElementById("comments-list");
-    commentsList.innerHTML = "";
+	const response = await getArticleComments(article_id);
+	const payload = JSON.parse(localStorage.getItem("payload"));
+	if (payload) {
+		currentUserId = payload.nickname;
+	} else {
+		currentUserId = null;
+	}
+	console.log(payload);
+	const commentsList = document.getElementById("comments-list");
+	commentsList.innerHTML = "";
 
 	// 댓글안에 링크찾아다가 하이퍼링크로 바꿔주기
-    function linkify(text) {
+	function linkify(text) {
 		const urlRegex = /(((https?:\/\/)|www\.)[^\s]+(\([^\s]+\)|[^\s.,!?:;\"'<>()\[\]\\/]|\/))/gi;
 		return text.replace(urlRegex, function (url) {
 			const href = url.startsWith("http") ? url : "http://" + url;
@@ -29,33 +29,33 @@ async function loadComments(article_id) {
 		});
 	}
 
-    response.forEach((comment) => {
-        let buttons = '';
-        console.log(comment.user);
-        console.log(currentUserId);
+	response.forEach((comment) => {
+		let buttons = '';
+		console.log(comment.user);
+		console.log(currentUserId);
 		console.log(comment.id)
 		console.log(comment)
-        if (currentUserId === comment.user) {
-            buttons = `
+		if (currentUserId === comment.user) {
+			buttons = `
             <div class="col d-grid gap-2 d-md-flex justify-content-end p-2 text-nowrap ">
                 <button type="button" class="btn btn-outline-secondary btn-sm" onclick="modifyComment(${comment.id}, '${comment.comment}')">수정</button>
                 <button type="button" class="btn btn-outline-secondary btn-sm" onclick="deleteComment(${comment.id})">삭제</button>
             </div>
             `;
-        }
+		}
 
-        commentsList.innerHTML += `
+		commentsList.innerHTML += `
         <li class="media d-flex align-items-center mt-2 mb-2 mr-2 border border-dark rounded">
 		<img class="img-thumbnail rounded-circle" src="https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288879.jpg" alt="profile img" width="50" height"50">
             <div class="media-body">
 			
                 <h6 class="mt-1 mb-1 ms-1 me-1">${comment.user}</h6>
-                <span class="mt-1 mb-1 ms-1 me-1">${linkify(comment.comment)}</span> <!-- 이 부분을 수정하여 링크 변환을 반영 -->
+                <span class="mt-1 mb-1 ms-1 me-1" style="word-break: break-all; white-space: pre-line;">${linkify(comment.comment)}</span> <!-- 이 부분을 수정하여 링크 변환을 반영 -->
             </div>
             ${buttons}
         </li>
         `;
-    });
+	});
 }
 
 // 게시글 상세보기 페이지가 로드될 때 실행되는 함수
@@ -103,18 +103,18 @@ window.onload = async function () {
 	}
 
 	// 댓글을 화면에 표시하기
-	const comments = await loadComments(article_id);
+	await loadComments(article_id);
 };
 
 // 댓글 등록 버튼
 async function submitComment() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const article_id = urlParams.get("article_id");
-    const commentElement = document.getElementById("new-comment")
-    const newComment = commentElement.value
-    console.log(`댓글 내용: ${newComment}`)
-    const response = await createComment(article_id, newComment)
-    commentElement.value = ""
+	const urlParams = new URLSearchParams(window.location.search);
+	const article_id = urlParams.get("article_id");
+	const commentElement = document.getElementById("new-comment")
+	const newComment = commentElement.value
+	console.log(`댓글 내용: ${newComment}`)
+	await createComment(article_id, newComment)
+	commentElement.value = ""
 
-    loadComments(article_id)
+	loadComments(article_id)
 }
